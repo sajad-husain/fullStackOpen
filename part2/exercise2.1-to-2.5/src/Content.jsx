e985d50329c9c0a944933238ad9e97b777f4bfb3
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-08-20 19:32:36 +0500

    reduced error debgging complete exercise 2.1 to 2.5 completed

--- part2/exercise2.1-to-2.5/src/Content.jsx
+++ part2/exercise2.1-to-2.5/src/Content.jsx
@@ -3,18 +3,22 @@ import Part from './Part'
 
 const Content = ({ courses }) => {
     console.log(courses);
-    const total = courses.reduce((sum, acc) => sum += acc.parts.exercises)
     return (
         <div>
-            {courses.map(course => (
-                <div key={course.id}>
-                    <strong>{course.name}</strong>
-                    {course.parts.map(part =>
-                        <Part key={part.id} name={part.name} exercises={part.exercises} />
-                    )}
-                    <strong>total of {total} exercises</strong>
-                </div>
-            ))}
+            {courses.map(course => {
+                const total = course.parts.reduce((sum, acc) => sum += acc.exercises, 0)
+                return (
+
+                    <div key={course.id}>
+                        <p><strong>{course.name}</strong></p>
+                        {course.parts.map(part =>
+                            <Part key={part.id} name={part.name} exercises={part.exercises} />
+                        )}
+                        <p><strong>total of {total} exercises</strong></p>
+                    </div>
+
+                )
+            })}
         </div>
     )
 }
