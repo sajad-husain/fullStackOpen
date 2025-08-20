69ce0bd91a7023720511d8737178fa06d3f67c77
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-08-20 14:34:52 +0500

    Header component rendered

--- part2/exercise2.1-to-2.5/src/App.jsx
+++ part2/exercise2.1-to-2.5/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Course from './components/Course'
+import Course from './Course'
 
 const App = () => {
   const course = {
--- part2/exercise2.1-to-2.5/src/Course.jsx
+++ part2/exercise2.1-to-2.5/src/Course.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const Course = ({ course }) => {
     return (
         <div>
+            <Header course={course} />
         </div>
     )
 }
