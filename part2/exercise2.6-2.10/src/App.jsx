82c970125d673f2fe613a4cd6a720768b694ed3a
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-08-25 23:52:20 +0500

    dummy data added to app to work on filtering dta

--- part2/exercise2.6-2.10/src/App.jsx
+++ part2/exercise2.6-2.10/src/App.jsx
@@ -3,7 +3,10 @@ import Details from './Details'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: "040-123456" }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -28,7 +31,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      filter shown with <input />
+      filter shown with <input onChange={filterHandler} />
       <h2>add a new</h2>
       <form onSubmit={addNameHandler}>
         <div>
