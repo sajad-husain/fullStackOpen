c8262404b60130407d59ab74161e6f34d5e2a3af
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-08-27 16:05:18 +0500

    refactoring app: form component created

--- part2/exercise2.6-2.10/src/App.jsx
+++ part2/exercise2.6-2.10/src/App.jsx
@@ -8,26 +8,12 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+
   const [input, setInput] = useState('')
   const [filteredData, setFilteredData] = useState([])
 
 
-  const addNameHandler = (event) => {
-    event.preventDefault()
-    const nameNumber = {
-      id: String(persons.length + 1),
-      name: newName,
-      number: newNumber
-    }
-    setPersons([...persons, nameNumber])
-    console.log(persons);
 
-    const matchedUser = persons.find(user => user.name === newName)
-    matchedUser ? alert(`${newName} already exists...`) : ''
-
-  }
 
   const personFilter = persons.filter(item => item.name.toLowerCase().includes(input))
   console.log('filtered persons', personFilter);
@@ -37,18 +23,7 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       filter shown with <input onChange={e => setInput(e.target.value)} value={input} />
-      <h2>add a new</h2>
-      <form onSubmit={addNameHandler}>
-        <div>
-          Name: <input onChange={(event) => setNewName(event.target.value)} value={newName} />
-          <br />
-          Number: <input onChange={(event) => setNewNumber(event.target.value)} value={newNumber} />
 
-        </div>
-        <div>
-          <button type='submit'>add</button>
-        </div>
-      </form>
       <h2>Numbers</h2>
       {personFilter.length === 0
         ? persons.map(person => <Details key={person.id} name={person.name} number={person.number} />)
---
+++ part2/exercise2.6-2.10/src/Form.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Form = ({ persons }) => {
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+
+    const addNameHandler = (event) => {
+        event.preventDefault()
+        const nameNumber = {
+            id: String(persons.length + 1),
+            name: newName,
+            number: newNumber
+        }
+        setPersons([...persons, nameNumber])
+        console.log(persons);
+
+        const matchedUser = persons.find(user => user.name === newName)
+        matchedUser ? alert(`${newName} already exists...`) : ''
+
+    }
+    return (
+        <div>
+            <h2>add a new</h2>
+            <form onSubmit={addNameHandler}>
+                <div>
+                    Name: <input onChange={(event) => setNewName(event.target.value)} value={newName} />
+                    <br />
+                    Number: <input onChange={(event) => setNewNumber(event.target.value)} value={newNumber} />
+
+                </div>
+                <div>
+                    <button type='submit'>add</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form
