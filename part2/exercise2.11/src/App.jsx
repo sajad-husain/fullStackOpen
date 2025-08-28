227dd4c5e36a4ed727be262e8eab5fcf55349b21
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-08-28 22:18:56 +0500

    axios installed and get request to fetch data also completed

--- part2/exercise2.11/src/App.jsx
+++ part2/exercise2.11/src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [persons, setPersons] = useState([])
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+      }
+      )
+  }, [])
+
+
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Phonebook</h1>
+      { }
+    </div>
   )
 }
 
