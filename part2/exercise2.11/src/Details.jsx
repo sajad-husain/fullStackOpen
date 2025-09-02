e474fa958d95027c2750ce2e33fdaea6b8ffa4b3
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-02 20:59:33 +0500

    delete butotn created in details componnents to delet phone book data

--- part2/exercise2.11/src/Details.jsx
+++ part2/exercise2.11/src/Details.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Details = ({ name, number }) => {
 
+    const deleteHandler = (id) => {
+        console.log('deleted');
+
+    }
     return (
         <div>
-            <p>{name} {number}</p>
+            <p>{name} {number} <button style={{ backgroundColor: 'skyblue', border: 'none', padding: '3px', borderRadius: '3px' }} onClick={() => deleteHandler(id)}>Delete</button></p>
         </div>
     )
 }
