3746af0d85851deeab83db33cabd5d24e80d0902
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-02 10:13:20 +0500

    data is being fetched from json server

--- part2/exercise2.11/src/App.jsx
+++ part2/exercise2.11/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Details from './Details'
 import Form from './Form'
 import FIlter from './FIlter'
@@ -6,16 +6,16 @@ import axios from 'axios'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [input, setInput] = useState('')
 
-  axios.get('http://localhost:3001/persons',)
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+    console.log('data is coming from json server');
+
+  }, [])
 
   const personFilter = persons.filter(item => item.name.toLowerCase().includes(input))
   console.log('filtered persons', personFilter);
